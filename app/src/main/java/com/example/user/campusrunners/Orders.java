6f92954ca2d8ccc08c534fae28d377f15599cc6f
gaj223 <nicheleaogungbayi@gmail.com>
2018-03-10 19:48:18 -0600

    Added more to the runner homepage

--- app/src/main/java/com/example/user/campusrunners/Orders.java
+++ app/src/main/java/com/example/user/campusrunners/Orders.java
@@ -2,16 +2,20 @@ package com.example.user.campusrunners;
 
 import java.util.ArrayList;
 
-// For all Open Orders
-public class OpenJob {
+/**
+ * Created by user on 3/10/18.
+ */
 
+public class Orders {
     public int orderId;
     public String businessName; // Name of the Business
+    public int buyerId;
+    public String buyerNote;
     public String date;
     public ArrayList<String> items; // item names
     public ArrayList<Integer> quantities; // quantity for each item
     public ArrayList<Float> prices; // price for each item
-    public int fee;
+    public String status; // active or completed
 
     // constructor with API Calls
     /*public OpenJob(int orderId){
@@ -22,16 +26,19 @@ public class OpenJob {
         this.buyerNote = note; // API Call to Database (Not Sure)
         this.quantities = quantities; // API Call to Database
         this.prices = prices; // API Call to Database
-        this.date = date; // API Call
+        this.date = date; // API Call\
+        this.st
 
     }*/
 
     // constructor - Temporary until API Calls are put
-    public OpenJob(int orderId, String business,
+    public Orders(int orderId, String business, int buyerId, String note,
                    ArrayList<String> items, ArrayList<Integer> quantities,
                    ArrayList<Float> prices, String date ){
         this.orderId = orderId;
         this.businessName = business;
+        this.buyerId = buyerId;
+        this.buyerNote = note;
         this.items = items;
         this.quantities = quantities;
         this.prices = prices;
@@ -46,19 +53,6 @@ public class OpenJob {
             total = total + quantities.get(i) * prices.get(i);
         }
         return total;
-
-    }
-
-    // Calculates the fee for the runner
-    public float getFee(){
-
-        float total = getTotal();
-        if (total > 15)
-            return 3;
-        else if (total > 10 )
-            return 2;
-        else
-            return 1;
 
     }
 }
--- app/src/main/java/com/example/user/campusrunners/RunnerHome.java
+++ app/src/main/java/com/example/user/campusrunners/RunnerHome.java
@@ -1,11 +1,17 @@
 package com.example.user.campusrunners;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class RunnerHome extends AppCompatActivity {
 
@@ -36,9 +42,79 @@ public class RunnerHome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_runner_home);
 
+        // Create bottom bar
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // Gets list of Open Orders
+        ArrayList<OpenJob> openOrders = addOrders();
+
+        // Add Orders (Buttons) to Screen
+        individualRequest(openOrders);
+
+
+
+
     }
+
+    // Get list of all the Current Orders
+    public ArrayList<OpenJob> addOrders(){
+        ArrayList<OpenJob> orders = new ArrayList<OpenJob>();
+        // API Call to get list of open orders
+        int orderNums[] = {200,201,202,203};
+        for (int i = 0; i < orderNums.length; i++){
+            //OpenJob job = new OpenJob(orders[i]);
+
+            // Delete when API Calls are added
+            ArrayList<String> items = new ArrayList<String>(); // item names
+            items.add("Pen, pack of 8");
+            items.add("Notebook");
+            ArrayList<Integer> quantities = new ArrayList<Integer>();; // quantity for each item
+            quantities.add(i+1);
+            quantities.add(i+2);
+            ArrayList<Float> prices = new ArrayList<Float>();// price for each item
+            prices.add(3.25f);
+            prices.add(1.00f);
+            OpenJob job = new OpenJob(orderNums[i], "Bookstore",items, quantities, prices, "3/15/2018" );
+
+            // add open job to array list
+            orders.add(job);
+        }
+
+        return orders;
+
+    }
+
+    // Create a button for each open request and add to the layout
+    public void individualRequest(ArrayList<OpenJob> jobs){
+
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);  //Can also be done in xml by android:orientation="vertical"
+        layout.setBackgroundColor(Color.rgb(255,140,0));
+        for (int i = 0; i < jobs.size(); i++) {
+            LinearLayout row = new LinearLayout(this);
+            OpenJob curr = jobs.get(i);
+            row.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+
+
+            Button btnTag = new Button(this);
+            btnTag.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+            btnTag.setText(curr.businessName + "\t\t\t$" + curr.getTotal() +
+                        " | $" + curr.getFee() + "\n" + curr.date);
+            btnTag.setId(i + 1);
+            btnTag.setWidth(1200);
+            btnTag.setHeight(400);
+            btnTag.setTextSize(24);
+            row.addView(btnTag);
+
+            layout.addView(row);
+
+        }
+        setContentView(layout);
+
+
+    }
+
 
 }
