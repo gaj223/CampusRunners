30ed820e02826c053eef7805a97b300fe9312c35
gaj223 <nicheleaogungbayi@gmail.com>
2018-03-21 15:07:34 -0500

    Added FAQ Page

---
+++ app/src/main/java/com/example/user/campusrunners/FAQ.java
@@ -0,0 +1,67 @@
+package com.example.user.campusrunners;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class FAQ extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_orders:
+                    mTextMessage.setText(R.string.title_orders);
+                    return true;
+                case R.id.navigation_profile:
+                    mTextMessage.setText(R.string.title_profile);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        // Allow user to navigate between activities
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent i;
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        i = new Intent(FAQ.this, RunnerHome.class);
+                        startActivity(i);
+                        break;
+                    case R.id.navigation_orders:
+                        i = new Intent(FAQ.this, ViewAllRunnerOrders.class);
+                        startActivity(i);
+                        break;
+                    case R.id.navigation_profile:
+                        // add later when Yadira creates profile page
+                        break;
+                }
+                return false;
+            }
+        });
+
+    }
+
+}
