09688f86fd207c4cd1215666feec90ab3ba56f51
yadiraH <yadira.hernandez021@yahoo.com>
2018-03-23 13:08:10 -0500

    Added a constants file to keep track of static variables

---
+++ app/src/main/java/com/example/user/campusrunners/Constants.java
@@ -0,0 +1,29 @@
+package com.example.user.campusrunners;
+
+/**
+ * Created by Yadi on 3/22/18.
+ */
+
+public class Constants {
+    private Constants() {
+        // restrict instantiation
+    }
+    //main api paths
+    public static final String LOCAL_PATH ="http://172.24.122.158:8000";
+    public static final String SERVER_PATH = "http://13.59.142.19/CampusRunnerBack/updated_apis";
+
+    //separate api calls
+    public static final String get_business_api="/business/get_business.php";
+    public static final String create_order_api="/order/order_create.php";
+    public static final String get_detail_order_api="/order/get_individual_order.php";
+    public static final String get_open_orders_api="/order/get_all_open_orders_runner.php";
+    public static final String update_orders_api="/order/update_order_status.php";
+    public static final String get_all_open_orders_api="/order/get_all_open_orders.php";
+    public static final String get_items_api ="/item/get_items.php";
+
+
+    //Order status
+    public static final String CLOSED = "CLOSED";
+    public static final String OPEN = "OPEN";
+    public static final String TRANS = "TRANS";
+}
