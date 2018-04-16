909c327251315ec713d48df62774f6a89637994b
gaj223 <nicheleaogungbayi@gmail.com>
2018-04-16 14:21:18 -0500

    Added rate the runner and complete button for buyer

--- app/src/main/java/com/example/user/campusrunners/RateTheRunner.java
+++ app/src/main/java/com/example/user/campusrunners/RateTheRunner.java
@@ -1,10 +1,13 @@
 package com.example.user.campusrunners;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 public class RateTheRunner extends AppCompatActivity {
@@ -20,11 +23,11 @@ public class RateTheRunner extends AppCompatActivity {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_orders:
+                    mTextMessage.setText(R.string.title_orders);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_profile:
+                    mTextMessage.setText(R.string.title_profile);
                     return true;
             }
             return false;
@@ -39,6 +42,40 @@ public class RateTheRunner extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // Allow user to navigate between activities
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent i;
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        i = new Intent(RateTheRunner.this, RunnerHome.class);
+                        startActivity(i);
+                        break;
+                    case R.id.navigation_orders:
+                        i = new Intent(RateTheRunner.this, ViewAllRunnerOrders.class);
+                        startActivity(i);
+                        break;
+                    case R.id.navigation_profile:
+                        // add later when Yadira creates profile page
+                        i = new Intent(RateTheRunner.this, RunnerProfile.class);
+                        startActivity(i);
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
+    // Takes the buyer to the homepage when they submit the runner's rating
+    public void submitRate(View v){
+        // Update runner's Rating by API Call
+        RatingBar runnerRate = (RatingBar) findViewById(R.id.ratingBar3);
+        Float runnerRating = runnerRate.getRating();
+
+        Intent submit = new Intent(RateTheRunner.this, RunnerHome.class);
+        startActivity(submit);
     }
 
 }
